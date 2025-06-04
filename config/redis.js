ec59a3be52b1c747a5b5d0d3e974c3435ba54bbb
shashwat anand <shashwat.anand@mediversal.in>
2025-06-04 09:49:01 +0530

    test

--- config/redis.js
+++ config/redis.js
@@ -1,18 +1,19 @@
-import { createClient } from 'redis';
-import dotenv from 'dotenv';
+const { createClient } = require('redis');
+const dotenv = require('dotenv');
 dotenv.config();
 
 const redisClient = createClient({
   socket: {
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
-    tls : true
+    tls: true
   },
-  database: 0, // Ensure this matches the database you are checking
+  database: 0,
 });
 redisClient.on('connect', () => console.log('Connected to Redis'));
 redisClient.on('error', (err) => console.error('Redis connection error:', err));
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
 
-await redisClient.connect();
-export default redisClient;
+redisClient.connect();
+
+module.exports = redisClient;
